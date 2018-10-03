3d6fe051bff8700ca2fe27e6a4e2743168cdaf58
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2018-10-03 16:26:21 -0700

    Implement configuration segment fetch API

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,6 +1,7 @@
 import { Product, DeserializedProduct } from '../core/models/product';
 import { ExtensionManifest } from '../core/models/manifest';
 import { toCamelCase } from '../util/case';
+import { createConfigurationToken } from './token';
 
 const API_HOST = process.env.API_HOST || 'api.twitch.tv';
 
@@ -210,4 +211,45 @@ export async function fetchNewRelease() {
     };
   }
   throw new Error('Cannot get GitHub developer rig latest release');
+}
+
+interface SegmentRecordMap {
+  [key: string]: {
+    record: ExtensionCoordinator.Segment;
+  }
+}
+
+export async function fetchGlobalConfigurationSegment(clientId: string, userId: string, secret: string): Promise<ExtensionCoordinator.Segment> {
+  const path = `/extensions/${clientId}/configurations/segments/global`;
+  const headers = {
+    Authorization: `Bearer ${createConfigurationToken(secret, userId)}`,
+    'Client-ID': clientId,
+  };
+  const segmentMap = await onlineApi.get<SegmentRecordMap>(path, 'Cannot authorize for global configuration', headers);
+  const global = segmentMap['global:'];
+  return global ? global.record : null;
+}
+
+export interface SegmentMap {
+  broadcaster?: ExtensionCoordinator.Segment;
+  developer?: ExtensionCoordinator.Segment;
+}
+
+export async function fetchChannelConfigurationSegments(clientId: string, userId: string, channelId: string, secret: string): Promise<SegmentMap> {
+  const path = `/extensions/${clientId}/configurations/channels/${channelId}`;
+  const headers = {
+    Authorization: `Bearer ${createConfigurationToken(secret, userId)}`,
+    'Client-ID': clientId,
+  };
+  const segmentMap = await onlineApi.get<SegmentRecordMap>(path, 'Cannot authorize for channel configurations', headers);
+  const broadcaster = segmentMap[`broadcaster:${channelId}`];
+  const developer = segmentMap[`developer:${channelId}`];
+  const segments = {} as SegmentMap;
+  if (broadcaster) {
+    segments.broadcaster = broadcaster.record;
+  }
+  if (developer) {
+    segments.developer = developer.record;
+  }
+  return segments;
 }
